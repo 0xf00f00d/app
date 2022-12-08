c155c14fa1a8db775f19a528acea61723fb3a435
MisterLobo <algae1234567890@gmail.com>
2022-12-08 23:49:55 +0800

    chore: added runtimeConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,6 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
     'nuxt-windicss',
     '@kevinmarrec/nuxt-pwa',
-    '@nuxtjs/apollo',
   ],
   pwa: {
     workbox: {
@@ -22,11 +21,12 @@ export default defineNuxtConfig({
   nitro: {
     preset: 'vercel',
   },
-  apollo: {
-    clients: {
-      default: {
-        httpEndpoint: process.env.NODE_ENV === 'production' ? 'https://edvtkshmocpxjwjlxyck.hasura.ap-southeast-1.nhost.run/v1/graphql' : 'http://localhost:1337/v1/graphql'
-      }
+  runtimeConfig: {
+    public: {
+      backendUrl: process.env.NODE_ENV === 'production' ? 'https://edvtkshmocpxjwjlxyck.hasura.ap-southeast-1.nhost.run/v1/graphql' : 'http://localhost:8080/v1/graphql',
+      subdomain: process.env.NODE_ENV === 'production' ? 'edvtkshmocpxjwjlxyck' : 'localhost',
+      region: process.env.NODE_ENV === 'production' ? 'ap-southeast-1' : '',
+      env: process.env.NODE_ENV,
     },
   },
 })
