2f74e9322c91c594db1d72979c5f0beb5933340e
MisterLobo <algae1234567890@gmail.com>
2022-12-09 23:16:06 +0800

    chore: update vuetify plugin

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,6 +1,7 @@
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { md3 } from 'vuetify/blueprints'
 
 export default defineNuxtPlugin((nuxt) => {
   const vuetify = createVuetify({
@@ -9,6 +10,7 @@ export default defineNuxtPlugin((nuxt) => {
     theme: {
       defaultTheme: 'dark',
     },
+    blueprint: md3,
   })
 
   nuxt.vueApp.use(vuetify)
